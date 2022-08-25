7469b908aca019eb318d8d5eee3daf594059aca2
eiloncohen <eiloncohen20@gmail.com>
2022-08-25 10:47:20 +0300

    edit commit
    edit commit of check_input_validation

--- Live.py
+++ Live.py
@@ -8,6 +8,13 @@ Here toy can find many cool games to play'''
 
 
 def check_input_validation(choose, start_range, end_range):
+    """
+    check_input_validation check if the input is valid or not
+    param choose: get the chosen value of player
+    param start_range: the first number of the range
+    param end_range: the last number of the range that exclude from range
+    return: if the input is valid or not
+    """
     if not choose.isdigit():
         return False
     elif int(choose) not in range(start_range, end_range):
